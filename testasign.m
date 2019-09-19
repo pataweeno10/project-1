95efc152980dd903f21fc3a7d31de18434921d73
pataweeno10 <43211989+pataweeno10@users.noreply.github.com>
2019-09-19 16:49:11 +0700

    test assign to 1 table

---
+++ testasign.m
@@ -0,0 +1,22 @@
+
+b=8
+row=3653
+col=8
+s=row*col
+day=1
+a=1
+first=335
+n=1
+a=2003:1:2018
+while(first<=425)
+    
+    test3(1,a:col)=hrhour1(first,:)
+    first=first+1
+    a=col+1
+    col=col+8
+    n=n+1
+end
+
+
+    
+ 
